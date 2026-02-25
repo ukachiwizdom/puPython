b408c687898318ce9a72e3e613bec793086d9c9b
Ukachi Wisdom <ukachiwizdom@gmai.com>
2026-02-25 13:50:01 +0100

    Add firstassignment.py

---
+++ firstassignment.py
@@ -0,0 +1,6 @@
+# Simple python program
+firstnumber = input("Enter first number: ")
+secondnumber = input("Enter second number: ")
+operation = input("Enter operation (+, -, *, /): ") 
+
+print(firstnumber, operation, secondnumber, "=", eval(firstnumber + operation + secondnumber))
